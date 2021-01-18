60f426f25f22701c69d91a687948bd5946a2694e
Tom Donohue <monodot@gmail.com>
2021-01-18 22:00:23 +0000

    Removed jade view dep because it was already out-of-date on creation - SHRUG

--- node-hello-world/app.js
+++ node-hello-world/app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,10 +8,6 @@ var greetingRouter = require('./routes/greeting');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,21 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/greeting', greetingRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- node-hello-world/routes/greeting.js
+++ node-hello-world/routes/greeting.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET greeting listing. */
+/* GET greeting */
 router.get('/', function(req, res, next) {
-  res.send({
-    "greeting": "Hello, world!"
-  });
+  res.send(
+    { "greeting": "Hello, world!" }
+  );
 });
 
 module.exports = router;
